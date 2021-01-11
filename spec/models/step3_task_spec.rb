f8faad30b49a55ca0694b08cedb35324c1b1f8f9
yuta <az7xj5xn@yahoo.co.jp>
2021-01-11 20:03:38 +0900

    検索機能のモデルスペックを追加

--- spec/models/step3_task_spec.rb
+++ spec/models/step3_task_spec.rb
@@ -15,11 +15,21 @@ RSpec.describe 'タスクモデル機能', type: :model do
     context 'scopeメソッドでステータス検索をした場合' do
       it "ステータスに完全一致するタスクが絞り込まれる" do
         # ここに内容を記載する
+        FactoryBot.create(:task, status: '完了')
+        search_tasks = Task.status_search('未着手')
+        expect(search_tasks.count).to eq 2
+        expect(search_tasks.status_search('完了').count).to eq 0
       end
     end
     context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
       it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
-        # ここに内容を記載する
+        #　検索に合致する
+        FactoryBot.create(:task, title:'task_title')
+        #　検索に合致しない
+        FactoryBot.create(:task, title:'task', status: '完了')
+        FactoryBot.create(:task, title:'sample')
+        search_tasks = Task.title_status_search('task', '未着手')
+        expect(search_tasks.count).to eq 2
       end
     end
   end
