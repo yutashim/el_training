e1eeb7a544797b7ad349bb89ff888af259fac6bc
yuta <az7xj5xn@yahoo.co.jp>
2021-01-14 15:23:55 +0900

    ユーザに対するアクセス制限を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-  def asc_desc_sort
+  def asc_desc_sort(tasks)
     if params[:sort_expired] == "ASC"
-      @tasks = Task.order(deadline: 'ASC')
+      @tasks = tasks.order(deadline: 'ASC')
       @order = "DESC"
     else
-      @tasks = Task.order(deadline: 'DESC')
+      @tasks = tasks.order(deadline: 'DESC')
       @order = "ASC"
     end
   end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,18 @@
 class TasksController < ApplicationController
   PER = 6
+  before_action :permit_tasks_page?
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :current_user
+  before_action :edit_task?, only: [:show, :edit, :update, :destroy]
   def index
-    redirect_to new_session_path unless @current_user
+    user_tasks = @current_user.tasks
     @order = "ASC"
-    @tasks = Task.order(created_at: 'DESC')
+    @tasks = user_tasks.order(created_at: 'DESC')
     if params[:sort_expired]
-      asc_desc_sort
+      asc_desc_sort(user_tasks)
     elsif params[:sort_priority]
-      @tasks = Task.order(priority: 'ASC')
+      @tasks = user_tasks.order(priority: 'ASC')
     elsif params[:search]
-      @tasks = Task.search_tasks(params[:search])
+      @tasks = user_tasks.search_tasks(params[:search])
     end
     @tasks = @tasks.page(params[:page]).per(PER)
   end
@@ -20,11 +21,11 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.new
+    @task = @current_user.tasks.new
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = @current_user.tasks.new(task_params)
     if @task.save
       flash[:notice] = 'タスクを作成しました'
       redirect_to task_path(@task.id)
@@ -59,4 +60,14 @@ class TasksController < ApplicationController
     params.require(:task).permit(:title, :detail, :deadline, :status, :priority)
   end
 
+  def permit_tasks_page?
+    current_user
+    redirect_to new_session_path unless @current_user
+  end
+
+  def edit_task?
+    unless @task.user_id == @current_user.id
+      redirect_to tasks_path
+    end
+  end
 end
