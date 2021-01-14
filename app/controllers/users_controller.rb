1f41ceb9694ddb1480a1363875b9cd61a046f0a3
yuta <az7xj5xn@yahoo.co.jp>
2021-01-14 13:48:55 +0900

    ユーザー登録機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :create, :show]
+  before_action :set_user, only: [:edit, :update, :show]
   def new
     @user = User.new
   end
@@ -32,5 +32,9 @@ class UsersController < ApplicationController
   private
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
