711b51624a3640779a110d1a1d94292a9daed3b3
yuta <az7xj5xn@yahoo.co.jp>
2021-01-14 13:26:04 +0900

    user_controllerの編集

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :create, :show]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "#{@user.name}さん、はじめまして！"
+      redirect_to user_path(@user.id)
+    else
+      render new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "#{@user.name}さん、プロフィールを変更しました！"
+      redirect_to user_path(@user.id)
+    else
+      render edit
+    end
+  end
+
+  def show
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
