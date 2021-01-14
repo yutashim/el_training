222f25e2e8f68d84b2de08f9823e45b4bb4e058e
yuta <az7xj5xn@yahoo.co.jp>
2021-01-14 21:49:20 +0900

    admin/user関連の更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,12 +43,17 @@ class UsersController < ApplicationController
   end
 
   def edit_user?
-    unless logged_in?
-      redirect_to new_user_path
-    else
-      if @current_user != @user
-        redirect_to user_path(@current_user.id)
-      end
+    # if logged_in?
+    #   redirect_to new_user_path
+    # else
+    #   if @current_user != @user
+    #     redirect_to user_path(@current_user.id)
+    #   end
+    # end
+    if !logged_in?
+      redirect_to new_session_path
+    elsif @current_user != @user && @current_user.id != 7
+      redirect_to user_path(@current_user.id)
     end
   end
 end
