e70675e468de3252bd059c26e7f2fff4f5febe8c
yuta <az7xj5xn@yahoo.co.jp>
2021-01-15 10:00:40 +0900

    n+1問題の解決

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,7 @@
 class Admin::UsersController < ApplicationController
   # before_action :admin_user?
   def index
-    @users = User.all
+    # @users = User.all
+    @users = User.includes(:tasks)
   end
 end
