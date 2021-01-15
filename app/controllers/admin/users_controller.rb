8692c55b0bdda7462d7a74502e496082191629b7
yuta <az7xj5xn@yahoo.co.jp>
2021-01-15 11:37:59 +0900

    管理者画面でロールの付与、削除ができるようにしました

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,19 @@
 class Admin::UsersController < ApplicationController
   before_action :admin_user?
+  before_action :set_user, only: [:edit, :destroy ]
   def index
     # @users = User.all
     @users = User.includes(:tasks)
+  end
+
+  def edit
+    @user.update(admin: true)
+    redirect_to admin_users_path
+  end
+
+  def destroy
+    @user.update(admin: false)
+    redirect_to admin_users_path
   end
 
   private
@@ -12,5 +23,9 @@ class Admin::UsersController < ApplicationController
       flash[:notice] = 'アクセス権限がありません'
       redirect_to user_path(@current_user.id)
     end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
