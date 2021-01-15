505aa023ed655ef2cdaebc1474eda4f0807d03a7
yuta <az7xj5xn@yahoo.co.jp>
2021-01-15 13:42:56 +0900

    adminユーザ消滅後に、adminユーザを生成するメソッドを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_secure_password
+  after_destroy :maintain_admin_user
+  after_update :maintain_admin_user
+  private
+  def maintain_admin_user
+    if User.find_by(admin: true) == nil
+      loop {
+        email = SecureRandom.alphanumeric(6)
+        user = User.new(
+          name: 'admin_user',
+          email: "#{email}@email.com",
+          password: 'password',
+          password_confirmation: 'password',
+          admin: true
+        )
+        break if user.save
+      }
+    end
+  end
 end
