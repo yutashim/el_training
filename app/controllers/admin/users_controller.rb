dc272aeb03dd02e5173e9e914ddc47cf880eb04a
yuta <az7xj5xn@yahoo.co.jp>
2021-01-15 11:09:29 +0900

    adminユーザ以外の管理画面へのアクセスを制御しました

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,16 @@
 class Admin::UsersController < ApplicationController
-  # before_action :admin_user?
+  before_action :admin_user?
   def index
     # @users = User.all
     @users = User.includes(:tasks)
+  end
+
+  private
+  def admin_user?
+    redirect_to new_session_path unless logged_in?
+    if @current_user.admin != true
+      flash[:notice] = 'アクセス権限がありません'
+      redirect_to user_path(@current_user.id)
+    end
   end
 end
