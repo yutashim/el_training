d154aae199f286469f4335d083f19f9ca6386264
yuta <az7xj5xn@yahoo.co.jp>
2021-01-16 10:25:05 +0900

    admin_userのコントローラの修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -18,10 +18,13 @@ class Admin::UsersController < ApplicationController
 
   private
   def admin_user?
-    redirect_to new_session_path unless logged_in?
-    if @current_user.admin != true
-      flash[:notice] = 'アクセス権限がありません'
-      redirect_to user_path(@current_user.id)
+    unless logged_in?
+        redirect_to new_session_path
+    else
+      if @current_user.admin != true
+        flash[:notice] = 'アクセス権限がありません'
+        redirect_to user_path(@current_user.id)
+      end
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 User.create(
   name: 'sample',
   email: 'sample@email.com',
+  admin: true
 )
